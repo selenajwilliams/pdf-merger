6b90dc45cfc54c5962d002559178fc7ea7502832
selenajwilliams <selena_williams@brown.edu>
2024-11-04 08:46:24 -0500

    added error checking to skip DS_Store file in input_files dir

--- merger.py
+++ merger.py
@@ -12,8 +12,12 @@ def merge_pdfs(in_path, files, outfile):
     print(f'merging files now...')
     # merge all the PDFs
     for pdf in files:
-        with open(f'{in_path}{pdf}', 'rb') as f:
-            merger.append(f)
+        try:
+            with open(f'{in_path}{pdf}', 'rb') as f:
+                merger.append(f)
+        except Exception as e:
+            print(f'Error encountered when attempting to merge {pdf}')
+            print(e)
 
     # write the merged PDF to a single output pdf
     # if the parent directory doesn't exist, create it 
@@ -34,10 +38,13 @@ def find_files(in_path) -> list[str]:
 
     files = []
     for file in os.listdir(in_path):
+        if file == ".DS_Store":
+            continue
         files.append(file)
         
     if len(files) == 0:
         raise Exception(f'No files were found in {in_path}/ directory')
+    
     return files
 
 if __name__ == "__main__":
@@ -46,4 +53,6 @@ if __name__ == "__main__":
 
     files = find_files(in_path) # a list of pdfs to merge
     files.sort()
+    print(f'Found the following files in input_files directory to merge...')
+    [print(f'   {x}') for x in files]
     merged = merge_pdfs(in_path, files, f"{out_path}/biotech_lectures.pdf")
